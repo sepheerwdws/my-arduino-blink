a671e2758659d095c42c12765883e792c3b4a888
Лиза <0012386@mail.ru>
2026-02-12 11:45:51 +0300

    Initial commit. Basic blink with 500ms delay

---
+++ Blink/Blink.ino
@@ -0,0 +1,9 @@
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(100000);                      // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  delay(100000);                      // wait for a second
+}
